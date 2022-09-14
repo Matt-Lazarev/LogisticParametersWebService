04ae6317b62077f76d47f24ab23c57b211fabd76
Лазарев Матвей Алексеевич <m.lazarev@utc.local>
2022-09-14 15:49:00 +0300

    Added Cross origin policy

--- src/main/java/com/uraltrans/logisticparamservice/controller/mvc/MapController.java
+++ src/main/java/com/uraltrans/logisticparamservice/controller/mvc/MapController.java
@@ -2,8 +2,10 @@ package com.uraltrans.logisticparamservice.controller.mvc;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@CrossOrigin("*")
 @Controller
 @RequiredArgsConstructor
 public class MapController {
