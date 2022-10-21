15932971101b0998ca21e0b57b654e999437add7
Лазарев Матвей Алексеевич <m.lazarev@utc.local>
2022-10-21 12:01:22 +0300

    Bug fixes

--- src/main/java/com/uraltrans/logisticparamservice/controller/api/FlightRestController.java
+++ src/main/java/com/uraltrans/logisticparamservice/controller/api/FlightRestController.java
@@ -2,8 +2,11 @@ package com.uraltrans.logisticparamservice.controller.api;
 
 import com.uraltrans.logisticparamservice.entity.postgres.Flight;
 import com.uraltrans.logisticparamservice.service.postgres.abstr.FlightService;
+import com.uraltrans.logisticparamservice.service.postgres.abstr.SecondEmptyFlightService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,9 +18,16 @@ import java.util.List;
 public class FlightRestController {
 
     private final FlightService flightService;
+    private final SecondEmptyFlightService secondEmptyFlightService;
 
     @GetMapping("/raw")
     public List<Flight> getAllFlights(){
         return flightService.getAllFlights();
+    }
+
+    @PostMapping("/second-empty-flight")
+    public ResponseEntity<?> saveSecondEmptyFlights(){
+        secondEmptyFlightService.saveAllSecondEmptyFlights();
+        return ResponseEntity.ok().build();
     }
 }
